8e5e7a394be822397292e8fbbc6bbf180e64d4fa
Iza Liberato <izaquela.liberato@gmail.com>
2023-05-18 15:21:56 -0300

    configs-docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemModule } from './item/item.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ItemModule],
+  imports: [ItemModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'admin',
+    password: 'admin',
+    database: 'postgres',
+    autoLoadEntities:true,
+    synchronize: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
+  
 })
 export class AppModule {}
